8f48839d34273fb8287bccb02bb01a9dddd3a0a4
Anna Wästling <45712410+AnnaWastling@users.noreply.github.com>
2021-11-16 01:21:56 +0100

    Camera spline

---
+++ Source/nDisplayDome/DomeAligned.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DomeAligned.h"
+
+// Sets default values
+ADomeAligned::ADomeAligned()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADomeAligned::BeginPlay()
+{
+	Super::BeginPlay();
+	DomeOrigin = GetActorLocation();
+	
+}
+
+// Called every frame
+void ADomeAligned::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	FVector2D LatCorr = FVector2D(-LatLon.X + (PI / 2.0), LatLon.Y);
+	FVector SphereLocation = LatCorr.SphericalToUnitCartesian();
+	SetActorLocation(DomeOrigin + SphereLocation * DomeRadius);
+
+}
+
